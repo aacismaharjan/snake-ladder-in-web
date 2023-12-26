c18e749e88bfcec1f58d53deeaba48d185b42532
Aashish Maharjan <aashishmaharjan2001@gmail.com>
2023-12-26 08:23:02 +0545

    Basic 1.1.5

--- js/app.js
+++ js/app.js
@@ -1,39 +1,4 @@
 
-class Board {
-    constructor(board, backgroundImage, SNAKES_AND_LADDERS) {
-        this.board = board;
-        this.backgroundImage = backgroundImage;
-        this.SNAKES_AND_LADDERS = SNAKES_AND_LADDERS;
-        this.board.style.backgroundImage = `url("${backgroundImage}")`;
-        this.playButton;
-
-    }
-
-    getBoard() {
-        return this.board;
-    }
-
-    setBoard(board) {
-        this.board = board;
-    }
-
-    getbackgroundImage() {
-        return this.backgroundImage;
-    }
-
-    setbackgroundImage(backgroundImage) {
-        this.backgroundImage = backgroundImage;
-        this.board.style.backgroundImage = `url("${backgroundImage}")`;
-    }
-
-    getSnakeAndLadders() {
-        return this.SNAKES_AND_LADDERS;
-    }
-
-    setSnakeAndLadders(SNAKES_AND_LADDERS) {
-        this.SNAKES_AND_LADDERS = SNAKES_AND_LADDERS;
-    }
-}
 
 
 class GameBoard {
